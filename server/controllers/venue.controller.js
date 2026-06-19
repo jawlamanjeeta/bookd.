e15746fbaab7238e9a458baba8f4c1344a5b5e79
jawlamanjeeta <jawlamanjeeta@gmail.com>
2026-06-19 06:38:05 +0530

    feat(controllers): add venue controller with filtering by type and city

---
+++ server/controllers/venue.controller.js
@@ -0,0 +1,64 @@
+const Venue = require('../models/Venue');
+
+const getAllVenues = async (req, res, next) => {
+  try {
+    const { type, city, capacity } = req.query;
+    const filter = { isActive: true };
+
+    if (type) filter.type = type;
+    if (city) filter['location.city'] = new RegExp(city, 'i');
+
+    const venues = await Venue.find(filter).populate('owner', 'name email');
+    res.json(venues);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getVenueById = async (req, res, next) => {
+  try {
+    const venue = await Venue.findById(req.params.id).populate('owner', 'name email');
+    if (!venue) return res.status(404).json({ message: 'Venue not found' });
+    res.json(venue);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const createVenue = async (req, res, next) => {
+  try {
+    const venue = await Venue.create({ ...req.body, owner: req.user._id });
+    res.status(201).json(venue);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateVenue = async (req, res, next) => {
+  try {
+    const venue = await Venue.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!venue) return res.status(404).json({ message: 'Venue not found' });
+    res.json(venue);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteVenue = async (req, res, next) => {
+  try {
+    const venue = await Venue.findByIdAndUpdate(
+      req.params.id,
+      { isActive: false },
+      { new: true }
+    );
+    if (!venue) return res.status(404).json({ message: 'Venue not found' });
+    res.json({ message: 'Venue deactivated' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getAllVenues, getVenueById, createVenue, updateVenue, deleteVenue };
