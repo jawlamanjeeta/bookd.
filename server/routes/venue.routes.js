e8372e8a71261bb89ba8be9625aaadb97c107675
jawlamanjeeta <jawlamanjeeta@gmail.com>
2026-06-21 16:47:03 +0530

    feat(routes): add /my and /:id/settings routes for venue owner

--- server/routes/venue.routes.js
+++ server/routes/venue.routes.js
@@ -6,13 +6,17 @@ const {
   createVenue,
   updateVenue,
   deleteVenue,
+  getMyVenues,
+  updateVenueSettings,
 } = require('../controllers/venue.controller');
 const { protect, adminOnly } = require('../middleware/auth.middleware');
 
 router.get('/', getAllVenues);
+router.get('/my', protect, getMyVenues);
 router.get('/:id', getVenueById);
 router.post('/', protect, createVenue);
 router.put('/:id', protect, adminOnly, updateVenue);
+router.patch('/:id/settings', protect, updateVenueSettings);
 router.delete('/:id', protect, adminOnly, deleteVenue);
 
 module.exports = router;
