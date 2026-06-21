e6db9aba931db36874020cf88e9a739a6e722983
jawlamanjeeta <jawlamanjeeta@gmail.com>
2026-06-21 17:34:45 +0530

    fix(app): remove duplicate dashboard route and move catch-all to end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,10 @@ import VenueDetail from './pages/VenueDetail'
 import BookingPage from './pages/BookingPage'
 import BookingSuccess from './pages/BookingSuccess'
 import MyBookings from './pages/MyBookings'
-import Dashboard from './pages/Dashboard'
 import OwnerDashboard from './pages/OwnerDashboard'
 import ResourceManagement from './pages/ResourceManagement'
 import ReservationManagement from './pages/ReservationManagement'
 import VenueSettings from './pages/VenueSettings'
-
 
 const App = () => {
   return (
@@ -29,12 +27,11 @@ const App = () => {
           <Route path="/booking/success" element={<ProtectedRoute><BookingSuccess /></ProtectedRoute>} />
           <Route path="/booking/:resourceId" element={<ProtectedRoute><BookingPage /></ProtectedRoute>} />
           <Route path="/my-bookings" element={<ProtectedRoute><MyBookings /></ProtectedRoute>} />
-          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-          <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/dashboard" element={<ProtectedRoute><OwnerDashboard /></ProtectedRoute>} />
           <Route path="/dashboard/resources" element={<ProtectedRoute><ResourceManagement /></ProtectedRoute>} />
           <Route path="/dashboard/reservations" element={<ProtectedRoute><ReservationManagement /></ProtectedRoute>} />
           <Route path="/dashboard/settings" element={<ProtectedRoute><VenueSettings /></ProtectedRoute>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
